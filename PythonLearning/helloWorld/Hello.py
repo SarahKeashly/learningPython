43e5cec8964befed5a6a458cd7f54a76327b5c2e
Sarah Keashly <sarah.keashly@makedigital.ca>
2022-08-09 15:43:33 -0700

    Learning python files

---
+++ PythonLearning/helloWorld/Hello.py
@@ -0,0 +1,23 @@
+# print ('''This is a multi-line string. This is the first line.
+# This is the second line.
+# "What's your name?," I asked.
+# He said "Bond, James Bond."
+# ''')
+#
+# age = 20
+# name = 'Swaroop'
+#
+# print('{0} was {1} years old when he wrote this book'.format(name, age))
+# print('Why is {0} playing with that python?'.format(name))
+#
+# print('a', end=' ')
+# print('b', end=' ')
+
+i = 5
+print(i)
+i = i + 1
+print(i)
+
+s = '''This is a multi-line string.
+This is the second line.'''
+print(s)
---
+++ PythonLearning/helloWorld/function_return.py
@@ -0,0 +1,18 @@
+def maximum(x, y):
+        if x > y:
+            return x
+        elif x == y:
+            return 'The numbers are equal'
+        else:
+            return y
+
+
+print(maximum(2,3))
+
+
+# returns 'none'
+# def some_function():
+#     pass
+#
+#
+# print(some_function())
